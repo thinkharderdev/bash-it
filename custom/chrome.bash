60b74f6d061d4b8c403e351350f1ff3aa2ad4ffe
Dan Harris <daniel.harris@asurion.com>
2020-04-30 07:07:47 -0400

    standardize profile folder names

--- custom/chrome.bash
+++ custom/chrome.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-alias chrome-anon='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="Profile 4" > /dev/null 2>&1 &'
-alias chrome-dan='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="Profile 3" > /dev/null 2>&1 &'
+alias chrome-anon='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="profile-anon" > /dev/null 2>&1 &'
+alias chrome-dan='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="profile-dan" > /dev/null 2>&1 &'
