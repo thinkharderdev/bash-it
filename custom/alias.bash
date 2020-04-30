0da08f6c983b1aa2db44e63462e36ff16be11c5b
thinkharderdev <1327726+thinkharderdev@users.noreply.github.com>
2020-04-30 06:42:58 -0400

    Add customiaton to source control

---
+++ custom/alias.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+alias chrome-anon='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="Profile 4" > /dev/null 2>&1 &'
+alias chrome-dan='nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="Profile 3" > /dev/null 2>&1 &'
---
+++ custom/exports.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:~/Tools/bin
+export BASH_SILENCE_DEPRECATION_WARNING=1
