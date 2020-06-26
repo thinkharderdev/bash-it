582b20c894b01f34dd897e57ad94e377d118640a
Dan Harris <dan@MacBook-Pro.local>
2020-06-26 05:41:01 -0400

    use chrome-private for chrome-anon

--- custom/chrome.bash
+++ custom/chrome.bash
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-alias chrome-anon='(nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="profile-anon" > /dev/null 2>&1 &)'
-alias chrome-dan='(nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory="profile-dan" > /dev/null 2>&1 &)'
+alias chrome-anon='(nohup chrome-private --delete > /dev/null 2>&1 &)'
